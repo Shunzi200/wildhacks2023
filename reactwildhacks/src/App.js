1f90b412fe6600db6242b06214272c19b850bb7c
neelk <neelkeswani@outlook.com>
2023-04-16 01:14:32 -0500

    server updates

--- reactwildhacks/src/App.js
+++ reactwildhacks/src/App.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ClimateChangeApp from './searchBar';
 import background1 from './assets/tree2.png';
 import Page1 from './page1';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  const variable = 'myVariable';
+
+  axios.get(`/api/data/${variable}`)
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
   return (
     <div>
+      <p>{!data ? "Loading..." : data}</p>
       <Page1 />
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -5,10 +5,48 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'your-database-hostname',
+  user: 'your-database-username',
+  password: 'your-database-password',
+  database: 'your-database-name'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+  } else {
+    console.log('Connected to database!');
+  }
+});
+
+// Example query
+connection.query('SELECT * FROM users', (err, results, fields) => {
+  if (err) {
+    console.error('Error querying database:', err);
+  } else {
+    console.log('Results:', results);
+  }
+});
+
+
+app.get('/api/data/:variable', (req, res) => {
+  const variable = req.params.variable;
+  // Use the variable to fetch data from your database or other data source
+  res.json({ data: variable });
+});
+
+
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    res.json({ message: "Hello!" });
   });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+//end mysql connection
+connection.end();
