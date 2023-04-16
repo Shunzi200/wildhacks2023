4c0118cc0fcba2e582643cdec95e588e6966ea36
Shunzi200 <ridet.samuel@gmail.com>
2023-04-15 19:36:44 -0500

    added background image and search bar

--- reactwildhacks/src/page1.js
+++ reactwildhacks/src/page1.js
@@ -3,26 +3,53 @@ import SearchBar from './searchBar';
 import background1 from './assets/tree2.png';
 
 function page1() {
-  return (
-    <div style={{ position: 'relative', backgroundColor: '#07bbf2', minHeight: '100vh' }}>
-      <img
-        src={background1}
-        alt="Background"
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: 'auto',
-          objectFit: 'cover',
-        }}
-      />
-
-      <div style={{ position: 'relative', zIndex: 1 }}>
-        <SearchBar />
+    return (
+      <div style={{ 
+        position: 'relative', 
+        backgroundColor: '#07bbf2', 
+        minHeight: '100vh', 
+        display: 'flex', 
+        flexDirection: 'column', 
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <img
+          src={background1}
+          alt="Background"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: 'auto',
+            objectFit: 'cover',
+          }}
+        />
+  
+        <div style={{ 
+          position: 'relative', 
+          zIndex: 1, 
+          display: 'flex', 
+          flexDirection: 'column', 
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          <h1 style={{ 
+            fontFamily: 'Lilita One',  
+            fontSize: 100, 
+            color: "white",
+            textAlign: 'center',
+            margin: 0,
+          }}>
+            Using data to <br/> solve climate change
+          </h1>
+  
+          <SearchBar style={{ marginTop: 20 }} />
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+  
+  
 
 export default page1;
--- reactwildhacks/src/searchBar.js
+++ reactwildhacks/src/searchBar.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import "./searchBar.css"
+import "./searchBarConnector.js"
 
-import './searchBar.css';
+const SearchBar = ({ data, onSearch }) => {
+  const [query, setQuery] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
 
-function searchBar() {
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setQuery(value);
+
+    const filtered = data.filter((item) =>
+      item.toLowerCase().includes(value.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
+
+  const handleSearch = () => {
+    onSearch(filteredData);
+  };
+
   return (
-    <div className="container">
-   
-      <h1 style={{ fontFamily: 'Montserrat',  fontWeight: 800, color: "white"}}>Using data to solve climate change</h1>
-      <input type="text" placeholder="Search..." />
+    <div className="search-bar">
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Search..."
+        value={query}
+        onChange={handleInputChange}
+      />
+      <button className="search-button" onClick={handleSearch}>
+        <FaSearch />
+      </button>
+      {filteredData.length > 0 && (
+        <ul className="search-results">
+          {filteredData.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
-export default searchBar;
+export default SearchBar;
