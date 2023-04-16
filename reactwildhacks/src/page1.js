2c4bf93d403958ed5bdb15bc00e42f89b4d5f087
Shunzi200 <ridet.samuel@gmail.com>
2023-04-15 21:01:03 -0500

    finalized first page

--- reactwildhacks/src/page1.js
+++ reactwildhacks/src/page1.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from "react";
+
 import SearchBar from './searchBar';
 import background1 from './assets/tree2.png';
+import countries from './countries.js';
 
-function page1() {
+function Page1() {
+    const [searchResults, setSearchResults] = useState([]);
+
+  // Define the country array
+
+
+  const handleSearch = (query) => {
+    // Filter the countries based on the search query
+    const filteredCountries = countries.filter((country) =>
+      country.toLowerCase().includes(query.toLowerCase())
+    );
+    // Update the search results
+    setSearchResults(filteredCountries);
+  };
+
     return (
       <div style={{ 
         position: 'relative', 
@@ -18,7 +34,8 @@ function page1() {
           alt="Background"
           style={{
             position: 'absolute',
-            top: 0,
+            top: 10,
+        
             left: 0,
             width: '100%',
             height: 'auto',
@@ -29,6 +46,7 @@ function page1() {
         <div style={{ 
           position: 'relative', 
           zIndex: 1, 
+          top: -150,
           display: 'flex', 
           flexDirection: 'column', 
           justifyContent: 'center',
@@ -44,12 +62,20 @@ function page1() {
             Using data to <br/> solve climate change
           </h1>
   
-          <SearchBar style={{ marginTop: 20 }} />
+           <SearchBar onSearch={handleSearch} countries={countries} />
+      <div className="search-results">
+        {searchResults.map((result) => (
+          <div key={result}>{result}</div>
+        ))}
+      </div>
         </div>
       </div>
+
+      
     );
   }
   
   
 
-export default page1;
+export default Page1;
+
--- reactwildhacks/src/searchBar.js
+++ reactwildhacks/src/searchBar.js
@@ -1,45 +1,56 @@
 import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import "./searchBar.css"
-import "./searchBarConnector.js"
+import "./searchBar.css";
 
-const SearchBar = ({ data, onSearch }) => {
+const SearchBar = ({ onSearch, countries }) => {
   const [query, setQuery] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
 
   const handleInputChange = (event) => {
-    const value = event.target.value;
-    setQuery(value);
-
-    const filtered = data.filter((item) =>
-      item.toLowerCase().includes(value.toLowerCase())
-    );
-    setFilteredData(filtered);
+    setQuery(event.target.value);
   };
 
   const handleSearch = () => {
-    onSearch(filteredData);
+    onSearch(query);
   };
 
+  let filteredCountries = [];
+
+  if (query.length > 0) {
+    filteredCountries = countries.filter((country) =>
+      country.toLowerCase().includes(query.toLowerCase())
+    );
+  }
+
   return (
-    <div className="search-bar">
-      <input
-        type="text"
-        className="search-input"
-        placeholder="Search..."
-        value={query}
-        onChange={handleInputChange}
-      />
-      <button className="search-button" onClick={handleSearch}>
-        <FaSearch />
-      </button>
-      {filteredData.length > 0 && (
-        <ul className="search-results">
-          {filteredData.map((item) => (
-            <li key={item}>{item}</li>
-          ))}
-        </ul>
-      )}
+    <div className="search-bar-container">
+      <div className="search-bar">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search..."
+          value={query}
+          onChange={handleInputChange}
+        />
+        <button className="search-button" onClick={handleSearch}>
+          <FaSearch />
+        </button>
+      </div>
+      <div className="search-results-container">
+        {filteredCountries.length > 0 && (
+          <div className="search-results">
+            <ul>
+              {filteredCountries.map((country) => (
+                 <div key={country}>{country}</div>
+              ))}
+            </ul>
+          </div>
+        )}
+        {query.length > 0 && filteredCountries.length === 0 && (
+          <div className="search-results">
+            <p>No results found.</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
